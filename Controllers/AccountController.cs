ae86d52a5b1c71662b3be3b5d2c28deaee492daa
0385246651 <haibaba2k@gmail.com>
2025-04-11 10:48:18 +0700

    Done Register

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeFirst.ViewModel;
+using EFCodeFirst.Models;
+using EFCodeFirst.Identity;
+using System.Web.Helpers;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace EFCodeFirst.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the user to the database
+                // Redirect to a success page or login page
+                var appDbContext = new AppDBContext();
+                var userStore = new AppUserStore(appDbContext);
+                var userManager = new AppUserManager(userStore);
+                var passwordHash = Crypto.HashPassword(model.Password);
+                var user = new AppUser()
+                {
+                    UserName = model.UserName,
+                    PasswordHash = passwordHash,
+                    Email = model.Email,
+                    PhoneNumber = model.Mobile,
+                    Birthday = model.DateOfBirth,
+                    Address = model.Address,
+                    City = model.City
+                };
+                IdentityResult identityResult = userManager.Create(user);
+                if(identityResult.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Customer");
+
+                    var authenManager = HttpContext.GetOwinContext().Authentication;
+
+                    var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                    authenManager.SignIn(new AuthenticationProperties(), userIdentity);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Please Enter Username and Password !.");
+                return View();
+            }
+        }
+    }
+}
---
+++ ViewModel/RegisterVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCodeFirst.ViewModel
+{
+	public class RegisterVM
+	{
+        [Required(ErrorMessage = "User Name is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
