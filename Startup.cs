bd5c20f378e7f85541204c1d88f6f6a0e19f8c46
0385246651 <haibaba2k@gmail.com>
2025-04-11 09:50:27 +0700

    init admin and manager account

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using Owin;
 
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity.EntityFramework;
+using EFCodeFirst.Identity;
 
 [assembly: OwinStartup(typeof(EFCodeFirst.Startup))]
 
@@ -14,12 +16,75 @@ namespace EFCodeFirst
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
-                
             });
+            this.CreateRolesAddUsers();
+        }
+
+        public void CreateRolesAddUsers()
+        {
+            var roleManager = new RoleManager<IdentityRole>(
+            new RoleStore<IdentityRole>(new AppDBContext()));
+            var appDbContext = new AppDBContext();
+            var appUserStore = new AppUserStore(appDbContext);
+            var userManager = new AppUserManager(appUserStore);
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+
+            if (userManager.FindByName("admin") ==null)
+            {
+                var user = new AppUser();
+                user.UserName = "admin";
+                user.Email = "admin@gmail.com";
+                string userPWD = "Admin@123";
+
+                var chkUser = userManager.Create(user, userPWD);
+                if (chkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+
+            }
+
+            //manage users
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+            }
+
+            if (userManager.FindByName("manager") == null)
+            {
+                var user = new AppUser();
+                user.UserName = "manager";
+                user.Email = "manager@gmail.com";
+                string userPWD = "manager@123";
+
+                var chkUser = userManager.Create(user, userPWD);
+                if (chkUser.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Manager");
+                }
+
+            }
+
+            //role user but not create user
+            //manage users
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
         }
     }
 }
